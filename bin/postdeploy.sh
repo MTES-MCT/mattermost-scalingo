0162045080847a3903323604b572627c55a04985
Edouard DAUBIN <edouard@7.fr>
2024-01-28 22:01:05 +0100

    Update postdeploy.sh

--- bin/postdeploy.sh
+++ bin/postdeploy.sh
@@ -4,8 +4,9 @@
 export MM_SERVICESETTINGS_LISTENADDRESS=":${PORT}"
 
 plugins_list=$(find /app/mattermost/postdeploy_plugins/. -maxdepth 1 -name '*.tar.gz' | tr '\n' ',')
-
+info $plugins_list
 for plugin in $(echo "$plugins_list" | tr ',' '\n')
 do
+  info $plugin
   /app/mattermost/bin/mattermost plugin add "$plugin"
 done
